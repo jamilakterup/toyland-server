72663d8e7abc64e17daac30a1699592301547e36
Md Jamil Akter <jamilakterup@gmail.com>
2023-05-21 02:19:34 +0600

    delete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {MongoClient, ServerApiVersion} = require('mongodb');
+const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
@@ -11,11 +11,11 @@ app.use(cors());
 
 
 
-const uri = 'mongodb://0.0.0.0:27017';
+// const uri = 'mongodb://0.0.0.0:27017';
 
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4rd33vy.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4rd33vy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,16 +30,6 @@ async function run() {
     try {
         // await client.connect();
 
-        // console.log(req.query);
-        // let query = {};
-        // if (req.query?.category || req.query?.subcategory) {
-        //     query = {
-        //         category: req.query.category,
-        //         subcategory: req.query.subcategory
-        //     }
-        // }
-
-
         const toyCollection = client.db('toylandDb').collection('toys');
 
         app.get('/toys', async (req, res) => {
@@ -50,9 +40,32 @@ async function run() {
 
         app.post('/toys', async (req, res) => {
             const body = req.body;
-            const results = await toyCollection.findOne(body);
-            res.send(results);
+            const result = await toyCollection.insertOne(body);
+            res.send(result);
         });
+
+        app.delete('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await toyCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        app.get('/toys/:subcategory', async (req, res) => {
+            const subcategory = req.params.subcategory;
+            const query = {subcategory: subcategory};
+            const result = await toyCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get('/myToys/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {email: email};
+            const result = await toyCollection.find(query).toArray();
+            res.send(result);
+        });
+
+
 
 
 
