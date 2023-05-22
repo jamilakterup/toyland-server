a2f1692b3298dae46808b7e6654585c569f8316e
Md Jamil Akter <jamilakterup@gmail.com>
2023-05-22 22:37:32 +0600

    vercel issue solve

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // await client.connect();
+        await client.connect();
 
         const toyCollection = client.db('toylandDb').collection('toys');
 
